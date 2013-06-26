95703b868554962b5a513138937f9612d1e167e0
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-06-26 12:54:55 +0800

    test

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello there...\n");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello from Windows Azure running node version: " + process.version + "</br>");
 }).listen(port);
