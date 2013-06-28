5be9b530cbfb5fa6e7a2a185ff627f4c523f69bc
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-06-28 15:23:04 +0800

    test

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+
+//app.enable("jsonp callback");
 
 app.use(function (err, req, res, next) {
     console.error(err.stack);
-    res.send(500, 'Something broke!');
+    res.send(500, "Something broke!");
 });
 
-app.get('/hello.txt', function (req, res) {
-    var body = 'Hello World';
-    res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Content-Length', body.length);
+app.get("/test01", function (req, res) {
+    var body = "Hello World";
+    res.setHeader("Content-Type", "text/plain");
+    res.setHeader("Content-Length", body.length);
     res.end(body);
 });
 
-app.get('/hello', function (req, res) {
-    res.send('Hello there');
+app.get("/test02", function (req, res) {
+    res.send("Hello there");
+});
+
+app.get("/test03", function (req, res) {
+    res.jsonp({ hello: "everybody" });
 });
 
 app.listen(process.env.PORT || 3000);
-console.log('Listening on port ' + (process.env.PORT || 3000));
+console.log("Express server listening on port %d in %s mode", process.env.PORT || 3000, app.settings.env);
